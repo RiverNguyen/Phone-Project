71735de1c930cb2d103fd1d23f000d6f365cfe89
RiverNguyen <ngiang1309@gmail.com>
2025-07-09 09:36:27 +0700

    Implement user redirection in login page based on authentication status
    - Updated the login page to check for an authenticated user using Supabase.
    - If a user is already logged in, they are redirected to the home page.
    - Converted the page component to an async function to handle the authentication check.

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,11 +1,19 @@
-import { LoginForm } from '@/components/login-form'
+import { LoginForm } from "@/components/login-form";
+import { createClient } from "@/lib/server";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (data?.user) {
+    redirect("/");
+  }
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
     </div>
-  )
+  );
 }
